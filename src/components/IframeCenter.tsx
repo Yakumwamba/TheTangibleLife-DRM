ca7e4e87a06ad03823794a754bd212fad7f07be6
yakumwamba <brianlemba2@gmail.com>
2022-04-21 23:47:20 +0200

    iframe screen resolution

--- src/components/IframeCenter.tsx
+++ src/components/IframeCenter.tsx
@@ -20,21 +20,20 @@ export default function IframeCenter() {
                 {!account ? <Flex direction={'column'} alignItems={'center'}>
                     <Text fontSize='2xl' fontWeight={'bold'} color={'white'}>TheTangibleLife DRM</Text>
                     <Text fontSize='lg' color={'white'} >This video is only available to NFT owners of TheTangibleLife NFT</Text>
-
                     <Text fontSize='lg' >To unlock this video, please connect your wallet</Text>
                     <Button onClick={handleConnectWallet} colorScheme={'green'} height="30px" > <Text fontSize='sm' fontWeight={'semibold'} color={'white'}>Connect Wallet</Text> </Button>
                 </Flex> : <Flex direction={'column'} alignItems={'center'}>
                     <Text fontSize='2xl' fontWeight={'bold'} color={'white'}> You are connected</Text>
                     <Text fontSize='sm' fontWeight={'bold'} color={'white'}> {account}</Text>
 
-                    <Box>
-                     
+                    <Box >
                         <iframe src="https://player.thetavideoapi.com/video/video_i32nq3tn23fia0br5cb4x90ir3"
-                           /* @ts-ignore */
+                            /* @ts-ignore */
                             border="0"
-                            width="100%"
-                            height="100%"
+                            width="680px"
+                            height="540px"
                             allowfullscreen />
+
                     </Box>
                 </Flex>}
 
