3969704efc6b7d1f24bd895756dd0cedf1089712
yakumwamba <brianlemba2@gmail.com>
2022-04-22 01:30:01 +0200

    only holders

--- src/components/IframeCenter.tsx
+++ src/components/IframeCenter.tsx
@@ -1,18 +1,489 @@
-import { Box, Button, Flex, Text } from '@chakra-ui/react'
-import { useEthers } from '@usedapp/core';
+import { Box, Button, Flex, Spacer, Text } from '@chakra-ui/react'
+import { useEtherBalance, useEthers } from '@usedapp/core';
 import React, { useEffect } from 'react'
+import Web3 from 'web3';
 
 export default function IframeCenter() {
 
     const { activateBrowserWallet, account } = useEthers();
+    const [hasNFT, setHasNFT] = React.useState(false);
+
     function handleConnectWallet() {
         activateBrowserWallet();
         console.log(account);
     }
     useEffect(() => {
         console.log(account);
+        if (account) {
+            handleCheckNFT();
+        }
+      
 
-    }, [account])
+    
+    }, [account, hasNFT])
+
+
+
+    const etherBalance = useEtherBalance(account);
+    let tangibleABI = [
+        {
+            "inputs": [
+                {
+                    "internalType": "string",
+                    "name": "name",
+                    "type": "string"
+                },
+                {
+                    "internalType": "string",
+                    "name": "symbol",
+                    "type": "string"
+                },
+                {
+                    "internalType": "string",
+                    "name": "uri",
+                    "type": "string"
+                }
+            ],
+            "stateMutability": "nonpayable",
+            "type": "constructor"
+        },
+        {
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "owner",
+                    "type": "address"
+                },
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "approved",
+                    "type": "address"
+                },
+                {
+                    "indexed": true,
+                    "internalType": "uint256",
+                    "name": "tokenId",
+                    "type": "uint256"
+                }
+            ],
+            "name": "Approval",
+            "type": "event"
+        },
+        {
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "owner",
+                    "type": "address"
+                },
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "operator",
+                    "type": "address"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "bool",
+                    "name": "approved",
+                    "type": "bool"
+                }
+            ],
+            "name": "ApprovalForAll",
+            "type": "event"
+        },
+        {
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "from",
+                    "type": "address"
+                },
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "to",
+                    "type": "address"
+                },
+                {
+                    "indexed": true,
+                    "internalType": "uint256",
+                    "name": "tokenId",
+                    "type": "uint256"
+                }
+            ],
+            "name": "Transfer",
+            "type": "event"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "to",
+                    "type": "address"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "tokenId",
+                    "type": "uint256"
+                }
+            ],
+            "name": "approve",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "owner",
+                    "type": "address"
+                }
+            ],
+            "name": "balanceOf",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "baseURI",
+            "outputs": [
+                {
+                    "internalType": "string",
+                    "name": "",
+                    "type": "string"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "tokenId",
+                    "type": "uint256"
+                }
+            ],
+            "name": "getApproved",
+            "outputs": [
+                {
+                    "internalType": "address",
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "owner",
+                    "type": "address"
+                },
+                {
+                    "internalType": "address",
+                    "name": "operator",
+                    "type": "address"
+                }
+            ],
+            "name": "isApprovedForAll",
+            "outputs": [
+                {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "name",
+            "outputs": [
+                {
+                    "internalType": "string",
+                    "name": "",
+                    "type": "string"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "tokenId",
+                    "type": "uint256"
+                }
+            ],
+            "name": "ownerOf",
+            "outputs": [
+                {
+                    "internalType": "address",
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "from",
+                    "type": "address"
+                },
+                {
+                    "internalType": "address",
+                    "name": "to",
+                    "type": "address"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "tokenId",
+                    "type": "uint256"
+                }
+            ],
+            "name": "safeTransferFrom",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "from",
+                    "type": "address"
+                },
+                {
+                    "internalType": "address",
+                    "name": "to",
+                    "type": "address"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "tokenId",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "bytes",
+                    "name": "_data",
+                    "type": "bytes"
+                }
+            ],
+            "name": "safeTransferFrom",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "operator",
+                    "type": "address"
+                },
+                {
+                    "internalType": "bool",
+                    "name": "approved",
+                    "type": "bool"
+                }
+            ],
+            "name": "setApprovalForAll",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "bytes4",
+                    "name": "interfaceId",
+                    "type": "bytes4"
+                }
+            ],
+            "name": "supportsInterface",
+            "outputs": [
+                {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "symbol",
+            "outputs": [
+                {
+                    "internalType": "string",
+                    "name": "",
+                    "type": "string"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "index",
+                    "type": "uint256"
+                }
+            ],
+            "name": "tokenByIndex",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "owner",
+                    "type": "address"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "index",
+                    "type": "uint256"
+                }
+            ],
+            "name": "tokenOfOwnerByIndex",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "tokenId",
+                    "type": "uint256"
+                }
+            ],
+            "name": "tokenURI",
+            "outputs": [
+                {
+                    "internalType": "string",
+                    "name": "",
+                    "type": "string"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "totalSupply",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "from",
+                    "type": "address"
+                },
+                {
+                    "internalType": "address",
+                    "name": "to",
+                    "type": "address"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "tokenId",
+                    "type": "uint256"
+                }
+            ],
+            "name": "transferFrom",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        }
+    ]
+
+    const tangibleContractAddress = '0x758972ce02de168d11103a473d97545bdd4d087c'
+    // const contract = new Contract(tangibleContractAddress, tangibleInterface)
+    // const { state, send } = useContractFunction(contract, 'tokenURI')
+    /// TangibleAddress 
+    const web3 = new Web3('https://eth-rpc-api-testnet.thetatoken.org/rpc')
+    const chainID = 361 // for the Theta Mainnet
+    // @ts-ignore
+    const contract = new web3.eth.Contract(tangibleABI, tangibleContractAddress)
+
+
+    function handleCheckNFT() {
+        // @ts-ignore
+        contract.methods.tokenURI(0).call((err, result) => {
+            console.log("TokenURI ===>", result,)
+        })
+        // @ts-ignore
+        contract.methods.balanceOf(`${account}`).call((err, result) => {
+            if (result > 0) {
+                setHasNFT(true)
+            }
+            console.log(`BalanceOf NFT for account  ${account} ===>`, result)
+        })
+        // @ts-ignore
+        // contract.methods.transferFrom("0x456B02f2BEb7CBd00A9A293de429187e3f110B1B", "0x159536D589b7885c58F83f8D3d9b2326668D6c0a", 0).call((err, result) => { 
+        //   console.log("Transfere the NFT to :",  ) 
+        // })
+
+        // // @ts-ignore
+        // contract.methods.transferFrom("0x456B02f2BEb7CBd00A9A293de429187e3f110B1B", "0x159536D589b7885c58F83f8D3d9b2326668D6c0a",0).call((err, result) => { 
+        //   if(err) {
+        //     console.log("Error:", err)
+        //   } 
+        //   console.log("Sending NFT:", result) 
+        // })
+    }
+
 
     return (
         <>
@@ -23,18 +494,28 @@ export default function IframeCenter() {
                     <Text fontSize='lg' >To unlock this video, please connect your wallet</Text>
                     <Button onClick={handleConnectWallet} colorScheme={'green'} height="30px" > <Text fontSize='sm' fontWeight={'semibold'} color={'white'}>Connect Wallet</Text> </Button>
                 </Flex> : <Flex direction={'column'} alignItems={'center'}>
-                    <Text fontSize='2xl' fontWeight={'bold'} color={'white'}> You are connected</Text>
-                    <Text fontSize='sm' fontWeight={'bold'} color={'white'}> {account}</Text>
+                    <Text fontSize='2xl' opacity={0.6} fontWeight={'bold'} color={'white'}> You are connected</Text>
 
-                    <Box >
+                    <Text fontSize='sm' opacity={0.4} fontWeight={'bold'} color={'white'}> {account}</Text>
+
+                    {hasNFT ? <Box >
                         <iframe src="https://player.thetavideoapi.com/video/video_i32nq3tn23fia0br5cb4x90ir3"
                             /* @ts-ignore */
                             border="0"
                             width="680px"
                             height="540px"
-                            allowfullscreen />
+                            allowFullScreen />
 
-                    </Box>
+                    </Box> :   
+                    
+                    <Flex alignItems={'center'} direction="column">
+                          <Spacer mt={'16px'} />
+                        <Text color={'#fff'} fontWeight="bold" fontSize='2xl' >Only NFT owners can watch - TangibleLife</Text>
+                        <Spacer mt={'8px'} />
+                        <Button textColor={'white'} alignSelf={'center'} bgGradient='linear(to-l, #7928CA, #FF0080)'>Buy NFT Now</Button>
+                    </Flex>
+                    }
+
                 </Flex>}
 
 
