f857f3b2d1cc21ffc63e6f2e57cdb7d42364c9cf
yakumwamba <brianlemba2@gmail.com>
2022-04-23 07:52:03 +0200

    spinner

--- src/components/UploadComponenet.tsx
+++ src/components/UploadComponenet.tsx
@@ -1,9 +1,9 @@
 import { Textarea, Text, Box, Button, Flex, Spacer, Input, Select, Divider, toast, useToast, Icon, Link } from "@chakra-ui/react"
 import React, { useCallback, useState } from "react"
 import { CopyIcon } from '@chakra-ui/icons'
-import LoadingOverlay from 'react-loading-overlay-ts';
+
 import GenerateEmbed from "./GenerateEmbed";
-import UploadButton from "./UploadButton";
+import UploadForm from "./UploadForm";
 import { FiVideo, FiSave, FiHelpCircle } from 'react-icons/fi'
 // @ts-ignore
 import { useLocation } from "react-router-dom";
@@ -13,51 +13,16 @@ export default function UploadVideo({ }) {
 
     const toast = useToast()
     // @ts-ignore
-    let handleInputChange = (e) => {
-        let inputValue = e.target.value
-        setValue(inputValue)
-
-    }
-
-    const [isActive, setActive] = useState(false)
+    
 
   
 
-    const handleButtonClicked = useCallback(() => {
-        setActive(value => !value)
+  
 
-        setTimeout(() => {
-
-            setActive(value => !value)
-            ///setVideoUrl("video_xxxxxxxxxxxxxxxxx")
-            toast({
-                title: `Video uploaded successfully`,
-                status: 'success',
-                isClosable: true,
-            })
-
-        }, 2000
-        )
-
-
-    }, [])
 
     return (
         <>
-            <LoadingOverlay
-                active={isActive}
-                spinner
-                text='Uploading Video please wait...'
-                // @ts-ignore
-                styles={{
-                    overlay: (base) => ({
-                        ...base,
-                        background: 'rgba(26, 32, 44, 0.7)'
-                    })
-                }}
-            >
-
-
+          
                 <Flex alignItems={'end'} flexDirection={
                     'column'
                 } width="100%" justify="space-between" >
@@ -67,28 +32,16 @@ export default function UploadVideo({ }) {
                     <Flex flexDirection={'row'} alignSelf={'start'} justify="space-between" alignItems={'center'} >
                         {/* create a button to for uploading file */}
                         {/* @ts-ignore */}
-
-                        <UploadButton />
-
+                        <UploadForm />
                     </Flex>
                     <Spacer mt={'8px'} />
-
-
-
-                
-                    
-
 
 
                     {/* <Text color={'white'}>{videoId}</Text> */}
 
                 </Flex>
 
-
-
-
-
-            </LoadingOverlay>
+         
 
         </>
     )
--- src/components/UploadForm.tsx
+++ src/components/UploadForm.tsx
@@ -8,6 +8,7 @@ import { useForm, UseFormRegisterReturn } from 'react-hook-form'
 import { FiVideo, FiSave, FiSend, FiHelpCircle } from 'react-icons/fi'
 import { supabase } from '../data/supabase'
 import VisuallyHidden from '@reach/visually-hidden'
+import LoadingOverlay from 'react-loading-overlay-ts';
 
 type FileUploadProps = {
   register: UseFormRegisterReturn
@@ -58,17 +59,13 @@ const UploadButton = () => {
   const [videoId, setVideoId] = useState(null)
   const [fileToUpload, setFile] = useState(null)
   const [uploaded, setUploaded] = useState(null)
-
+  const [isActive, setActive] = useState(false)
   const [supabaseURl, setSupabaseURL] = useState(null)
 
-
-
   const onSubmit = handleSubmit((data) => {
-
-
-    
     //uploadVideoViaPresignedUrl(data.file_)
-     uploadToSupabase(fileToUpload)
+
+    uploadToSupabase(fileToUpload)
 
   })
 
@@ -79,7 +76,7 @@ const UploadButton = () => {
     }
     for (const videoFile of Array.from(value)) {
       const fsMb = videoFile.size / (1024 * 1024)
-      const MAX_FILE_SIZE =  100
+      const MAX_FILE_SIZE = 20
       if (!fileToUpload || fileToUpload === null) {
         // @ts-ignore
         setFile(videoFile)
@@ -87,7 +84,7 @@ const UploadButton = () => {
       }
 
       if (fsMb > MAX_FILE_SIZE) {
-        return 'Max file size 100mb'
+        return 'Max file size 20mb'
       }
 
 
@@ -97,11 +94,12 @@ const UploadButton = () => {
 
   //@ts-ignore
   const uploadToSupabase = async (file) => {
+    setActive(true)
     try {
       const fileExt = file.name.split('.').pop()
       const fileName = `${Math.random()}.${fileExt}`
       const filePath = `${fileName}`
-// @ts-ignore
+      // @ts-ignore
       let { uploadError } = await supabase.storage.from('tangible-bucket').upload(filePath, file).then(async result => {
         console.log(result)
         // @ts-ignore
@@ -111,27 +109,27 @@ const UploadButton = () => {
 
 
         // @ts-ignore
-        if(result.data.Key) {
-          console.log("Created the url =========>" , result)
+        if (result.data.Key) {
+          console.log("Created the url =========>", result)
           // @ts-ignore
           //setUploaded(result.data.Key)
-       await supabase
-          .storage
-          .from('tangible-bucket')
-          //@ts-ignore
-          .createSignedUrl(result.data.Key.split('/').pop(), 60).then(async result => {
-  
-            console.log(result.data?.signedURL)
-            // @ts-ignore
-            setSupabaseURL(result.data?.signedURL)
+          await supabase
+            .storage
+            .from('tangible-bucket')
+            //@ts-ignore
+            .createSignedUrl(result.data.Key.split('/').pop(), 60).then(async result => {
 
-          })
-  
+              console.log(result.data?.signedURL)
+              // @ts-ignore
+              setSupabaseURL(result.data?.signedURL)
+              setActive(false)
+            })
+
           // @ts-ignore
           console.log(result.data.Key)
         }
 
-     
+
 
 
 
@@ -142,7 +140,7 @@ const UploadButton = () => {
 
       if (uploadError) {
         console.log("Supabase Error while uploading => ", uploadError)
-        
+
       }
       console.log("Uploaded to supabase")
 
@@ -152,7 +150,7 @@ const UploadButton = () => {
       // onUpload(filePath)
       // $ts-ignore
     } catch (error) {
-      
+
     } finally {
       // setUploading(false)
     }
@@ -186,10 +184,6 @@ const UploadButton = () => {
     }
     )
   }
-
-
-
-
   // @ts-ignore
   async function transcodeVideoExternalUrl(url) {
     var options = {
@@ -209,18 +203,18 @@ const UploadButton = () => {
     await fetch(options.url, options).then(async (response) => {
       console.log("Transcoding Video please wait......")
       const resp = await response.json()
-       const videoId = resp.body.videos[0].id
+      const videoId = resp.body.videos[0].id
       console.log(resp)
-      console.log("Setting Video id to " , videoId)
+      console.log("Setting Video id to ", videoId)
       // @ts-ignore
       setVideoId(videoId)
-      
-    
-    
+
+
+
       //setVideoUrl( response.json())
 
-          }).then(async (data) => {
-     
+    }).then(async (data) => {
+
     }
     ).catch(err => {
       console.log("Error while Transcoding Video from external URL", err)
@@ -232,7 +226,7 @@ const UploadButton = () => {
     //     await checkProgress(videoId)
     //   }, 5000)
     // }
-   
+
 
   }
 
@@ -247,7 +241,7 @@ const UploadButton = () => {
         'x-tva-sa-id': `${id}`,
         'x-tva-sa-secret': `${secrete}`
       },
-    
+
 
     };
 
@@ -255,8 +249,8 @@ const UploadButton = () => {
       console.log("Checking progress for video id ....." + videoId)
       const resp = await response.json()
       console.log("====== Progress REsponse ======== ", resp.body.videos)
-      if(resp.body.videos[0].progress == 99.9) {
-        console.log("Uploading to Theta Network almost done:" , resp.body.videos[0].progress)
+      if (resp.body.videos[0].progress == 99.9) {
+        console.log("Uploading to Theta Network almost done:", resp.body.videos[0].progress)
       }
       //setVideoUrl( response.json())
 
@@ -264,15 +258,12 @@ const UploadButton = () => {
       console.log(data);
     }
     ).catch(err => {
-    console.log("Error in checking the video Pogress", err)
+      console.log("Error in checking the video Pogress", err)
     }
     )
   }
 
   async function requestPresignedUrl() {
-
-   
-
     var options = {
       'method': 'POST',
       'url': 'https://api.thetavideoapi.com/upload',
@@ -296,93 +287,110 @@ const UploadButton = () => {
     )
 
   }
-// @ts-ignore
-  useEffect(async () => {
+  // @ts-ignore
+  useEffect(() => {
     // console.log("File to upload is ", fileToUpload)
     // // uploadVideoViaPresignedUrl(fileToUpload)
 
-    if(supabaseURl) {
+    if (supabaseURl) {
       console.log("Awaiting video Transcoding...")
       console.log("External URL to be transcoded is ", supabaseURl)
-      await transcodeVideoExternalUrl(supabaseURl)
+      transcodeVideoExternalUrl(supabaseURl)
     }
 
-     
   }, [supabaseURl])
+
 
   return (
     <>
-      <form onSubmit={onSubmit}>
-        <FormControl isInvalid={!!errors.file_} isRequired>
+      <LoadingOverlay
+        active={isActive}
+        spinner
+        text='Uploading Video please wait...'
+        // @ts-ignore
+        styles={{
+          overlay: (base) => ({
+            ...base,
+            background: 'rgba(26, 32, 44, 0.7)'
+          })
+        }}
+      >
+        <form onSubmit={onSubmit}>
+          <FormControl isInvalid={!!errors.file_} isRequired>
 
-          <FileUpload
-            accept={'image/*'}
-            multiple
-            register={register('file_', { validate: validateFiles })}
-          >
+            <FileUpload
+              accept={'video/*'}
+              multiple
+              register={register('file_', { validate: validateFiles })}
+            >
 
-            <Button
-              // @ts-ignore
-              type='file'
-              id="single"
-              accept="video/*"
-              onChange={(event) => {
-                alert("File selected")
-              }}
-              onClick={() => {
-                //requestPresignedUrl()
-              }} bgGradient='linear(to-r, teal.500, green.500)' textColor={'white'} leftIcon={<Icon as={FiVideo} />}>
-              Upload video
-            </Button>
+              <Button
+                // @ts-ignore
+                type='file'
+                id="single"
+                accept="video/*"
+                onChange={(event) => {
+                  alert("File selected")
+                }}
+                onClick={() => {
+                  //requestPresignedUrl()
+                }} bgGradient='linear(to-r, teal.500, green.500)' textColor={'white'} leftIcon={<Icon as={FiVideo} />}>
+                Upload video
+              </Button>
 
-          </FileUpload>
+            </FileUpload>
 
-          <Text color={'white'}>
-            {errors.file_ && errors?.file_.message}
-          </Text>
-
-          <Flex flexDirection={'row'} w={'500px'} alignSelf={'start'} justify="space-between" alignItems={'center'} >
-            <Text fontStyle={'italic'} color={'white'} mb='8px'>(Optional) Hash of any Required NFT (see how)  <Icon color={'white'} h={6} w={6} as={FiHelpCircle} />
+            <Text color={'white'}>
+              {errors.file_ && errors?.file_.message}
             </Text>
-            {/* @ts-ignore */}
 
-          </Flex>
-          <Spacer mt={'8px'} />
-          <Input padding={'4px'} textColor={'white'} variant='outline' placeholder='0x1234...' />
+            <Flex flexDirection={'row'} w={'500px'} alignSelf={'start'} justify="space-between" alignItems={'center'} >
+              <Text fontStyle={'italic'} color={'white'} mb='8px'>(Optional) Hash of any Required NFT (see how)  <Icon color={'white'} h={6} w={6} as={FiHelpCircle} />
+              </Text>
+              {/* @ts-ignore */}
 
-          <Flex flexDirection={'row'} alignSelf={'start'} justify="space-between" alignItems={'center'} >
-            <Text color={'white'} mb='8px'>Select Dimensions
-            </Text>
-          </Flex>
+            </Flex>
+            <Spacer mt={'8px'} />
+            <Input padding={'4px'} textColor={'white'} variant='outline' placeholder='0x1234...' />
 
-          <Select padding={'4px'} placeholder='560 x 315' size='md' color={'gray'} borderRadius={'6px'}>
-            <option value='small'>640 x 480</option>
-            <option value='medium'>1280 x 960</option>
-            <option value='large'>1920 x 1080</option>
-          </Select>
-          <Spacer mt={'8px'} />
-          <Flex direction={'row'}justifyContent={'end'} > 
-          <Button
-            alignSelf={'end'}
-            
-            // isLoading
-            // loadingText='Submitting'
-            rightIcon={<Icon color={'white'} h={6} w={6} as={FiSave} />}
-            colorScheme='green'
-            // @ts-ignore
-            bgColor={'#F88487'}
-            justifyContent={'center'}
-            fontWeight={'semibold'}
-           type='submit'
-          >
-            {/* <Link href='/generated-iframe' >Submit </Link> */} Upload
-          </Button>
-          </Flex>
-          
-        </FormControl>
+            <Flex flexDirection={'row'} alignSelf={'start'} justify="space-between" alignItems={'center'} >
+              <Text color={'white'} mb='8px'>Select Dimensions
+              </Text>
+            </Flex>
+
+            <Select padding={'4px'} placeholder='560 x 315' size='md' color={'gray'} borderRadius={'6px'}>
+              <option value='small'>640 x 480</option>
+              <option value='medium'>1280 x 960</option>
+              <option value='large'>1920 x 1080</option>
+            </Select>
+            <Spacer mt={'8px'} />
+            <Flex direction={'row'} justifyContent={'end'} >
+              <Button
+                alignSelf={'end'}
+
+                // isLoading
+                // loadingText='Submitting'
+                rightIcon={<Icon color={'white'} h={6} w={6} as={FiSave} />}
+                colorScheme='green'
+                // @ts-ignore
+                bgColor={'#F88487'}
+                onClick={() => {
+                  onSubmit()
+                }
+                }
+                justifyContent={'center'}
+                fontWeight={'semibold'}
+                
+              >
+                {/* <Link href='/generated-iframe' >Submit </Link> */} Upload
+              </Button>
+            </Flex>
+
+          </FormControl>
 
 
-      </form>
+        </form>
+      </LoadingOverlay>
     </>
   )
 }
